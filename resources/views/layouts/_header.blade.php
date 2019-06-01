b3b8f8253c0c58cd67dacd4451fa9faf88f3fba8
zjgyu <85028307@qq.com>
2019-06-01 08:54:09 +0000

    命名路由

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a href="/" class="navbar-brand">Weibo App</a>
+        <a href="{{route('home')}}" class="navbar-brand">Weibo App</a>
         <ul class="navbar-nav justify-content-end">
-            <li class="nav-item"><a href="/help" class="nav-link">帮助</a></li>
+            <li class="nav-item"><a href="{{route('help')}}" class="nav-link">帮助</a></li>
             <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
         </ul>
     </div>
